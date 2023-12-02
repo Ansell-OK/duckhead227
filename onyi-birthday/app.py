d28ee7c6302cae382ad6134735ccaf7ce09ffad1
Ans-helm <isolumgolie@gmail.com>
2023-12-03 00:20:58 +0100

    birthday

---
+++ onyi-birthday/app.py
@@ -0,0 +1,19 @@
+import streamlit as st 
+
+st.write("""
+## **CLICK ME, BITCH**
+""")
+
+click = st.button('CLICK ME 🎂')
+
+if click: 
+    st.write("""
+## HAPPY BIRTHDAY BABY🎂2️⃣0️⃣
+
+It's been a while since I wrote on of these so I am **rusty**. This is short notice but i felt like working on this little project while i code. I love you, I pray as you age gracefully
+you continue to be a light in the darkness not just for yourself but for an entire generation looking up to you. I believe in your abilities and capabilities outside the square circle 
+that is school and i am confifent that you will age gracefully and powerfully in the Light of the Lord. You have grown into a beautiful young lady with dreams that rival Joseph's. I can 
+only pray your dreams become reality. Happy 20. I love you and Have a wonderful cake day. 
+             
+PS: *I ordered new jewelry for you, they're not here yet😔 I think its a shipping thing*
+""")
